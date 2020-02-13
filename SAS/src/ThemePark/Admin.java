a3623be5c2cd55154cb6ca3068d4c2fbcf792231
Subair AS <sincenov1998@gmail.com>
2020-02-13 13:23:48 +0530

    In this change, I can add rides station without terminate concole

---
+++ SAS/src/ThemePark/Admin.java
@@ -0,0 +1,81 @@
+package ThemePark;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Admin {
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void actionDecider() {
+		adminlabel: {
+			while (true) {
+				label: {
+					while (true) {
+						System.out.println("1.Add new Ride?\n2.Check - Revenue for the day\n3.Exit");
+						int selection = sc.nextInt();
+						switch (selection) {
+						case 1:
+							addRide();
+							break label;
+						case 2:
+							checkRevenue();
+							break label;
+						case 3:
+							break adminlabel;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private static void checkRevenue() {
+		System.out.println("1.Ticket Counter\n2.Canteen\n3.Overall");
+		switch (sc.nextInt()) {
+		case 1:
+			System.out.println(TicketCounter.getRevenue());
+			break;
+		case 2:
+			System.out.println(Canteen.getRevenue());
+			break;
+		case 3:
+			System.out.println(TicketCounter.getRevenue() + Canteen.getRevenue());
+			break;
+		}
+	}
+
+	private static void addRide() {
+		System.out.println("1.RollerCoaster Station\n2.FlashTower Station\n3.New Station");
+		int selection = sc.nextInt();
+		System.out.println("Max Capacity?");
+		int maxCapacity = sc.nextInt();
+		switch (selection) {
+		case 1:
+			RidesManagerDataBase.addNewRide(new RollerCoasterStation(maxCapacity));
+			break;
+		case 2:
+			RidesManagerDataBase.addNewRide(new FlashTowerStation(maxCapacity));
+			break;
+//		case 3:
+//			System.out.println("Ride Name?");
+//			String rideName = sc.nextLine();
+//			System.out.println("Min age alloted");
+//			int age = sc.nextInt();
+//			System.out.println("Min height alloted");
+//			int height = sc.nextInt();
+//			System.out.println("Max weight alloted");
+//			int weight = sc.nextInt();
+//			System.out.println("Start time? eg: for 1PM - 1300");
+//			int startTime = sc.nextInt();
+//			System.out.println("End Time?");
+//			int endTime = sc.nextInt();
+//			System.out.println("Single Ride Time?");
+//			int singleRideTime = sc.nextInt();
+//			RidesManagerDataBase.addNewRide(new NewStation.NewStationBuilder().setAge(age).setHeight(height)
+//					.setStartTime(LocalTime.of(startTime / 100, startTime % 100))
+//					.setEndTime(LocalTime.of(endTime / 100, endTime % 100))
+//					.setSingleRideTime(LocalTime.of(singleRideTime / 100, singleRideTime % 100)).setWeight(weight)
+//					.setMaxCapacity(maxCapacity).setRideName(rideName).getNewStationInstance());
+//			break;
+		}
+	}
+}
---
+++ SAS/src/ThemePark/NewStation.java
@@ -0,0 +1,77 @@
+//package ThemePark;
+//
+//import java.time.LocalTime;
+//
+//public class NewStation extends RidesStation {
+//	private int age, height, weight;
+//
+//	public NewStation(int age, int height, int weight, int maxCapacity, LocalTime startTime, LocalTime endTime,
+//			LocalTime singleRideTime, String rideName) {
+//		this.age = age;
+//		this.height = height;
+//		this.weight = weight;
+//		super.setMaxCapacity(maxCapacity);
+//		super.setStartTime(startTime);
+//		super.setEndTime(endTime);
+//		super.setSingleRideTime(singleRideTime);
+//		super.setRideName(rideName);
+//	}
+//
+//	public boolean canVisitorRide(Visitor currentVisitor) {
+//		if (age < currentVisitor.getAge() && height < currentVisitor.getHeight()
+//				&& weight > currentVisitor.getWeight()) {
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	public static class NewStationBuilder {
+//		private int age, height, weight, maxCapacity;
+//		private LocalTime startTime, endTime, singleRideTime;
+//		private String rideName;
+//
+//		public NewStationBuilder setAge(int age) {
+//			this.age = age;
+//			return this;
+//		}
+//
+//		public NewStationBuilder setHeight(int height) {
+//			this.height = height;
+//			return this;
+//		}
+//
+//		public NewStationBuilder setWeight(int weight) {
+//			this.weight = weight;
+//			return this;
+//		}
+//
+//		public NewStationBuilder setMaxCapacity(int maxCapacity) {
+//			this.maxCapacity = maxCapacity;
+//			return this;
+//		}
+//
+//		public NewStationBuilder setStartTime(LocalTime startTime) {
+//			this.startTime = startTime;
+//			return this;
+//		}
+//
+//		public NewStationBuilder setEndTime(LocalTime endTime) {
+//			this.endTime = endTime;
+//			return this;
+//		}
+//
+//		public NewStationBuilder setSingleRideTime(LocalTime singleRideTime) {
+//			this.singleRideTime = singleRideTime;
+//			return this;
+//		}
+//
+//		public NewStationBuilder setRideName(String rideName) {
+//			this.rideName = rideName;
+//			return this;
+//		}
+//
+//		public NewStation getNewStationInstance() {
+//			return new NewStation(age, height, weight, maxCapacity, startTime, endTime, singleRideTime, rideName);
+//		}
+//	}
+//}
